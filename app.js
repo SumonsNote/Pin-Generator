6a47465fda5cf533ac89ac3d6b59ec7cab20407a
Sumonsnote <sohankhance@gmail.com>
2022-02-13 15:33:03 +0600

    finished project pin generator

--- app.js
+++ app.js
@@ -13,3 +13,34 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+})
+
+function notifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const failedMsg = document.getElementById('failed-msg');
+    const successMsg = document.getElementById('success-msg');
+    
+    if (pin == typedNumbers) {
+        successMsg.style.display = 'block';
+        failedMsg.style.display = 'none';
+    }
+    else {
+        successMsg.style.display = 'none';
+        failedMsg.style.display = 'block';
+    }
+}
